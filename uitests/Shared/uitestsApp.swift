1fd85056eaab61011b5e05adf10b89db64ee00b2
Aileen B. Romero <devaileenromero@gmail.com>
2021-06-08 16:06:41 +0800

    create project

---
+++ uitests/Shared/uitestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestsApp.swift
+//  Shared
+//
+//  Created by Aileen Romero on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct uitestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
